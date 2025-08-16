3f336c278415d63fe214c4ea92844a933acf3117
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2025-08-16 20:24:25 +0000

    Convert bin to uf2 for softwareless installation

--- KeyboardAdapter21_samd21-usbhost/make-KeyboardAdapter21.sh
+++ KeyboardAdapter21_samd21-usbhost/make-KeyboardAdapter21.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-arduino-cli compile --output-dir . --fqbn adafruit:samd:adafruit_feather_m0_express KeyboardAdapter21
+arduino-cli compile --output-dir . --fqbn adafruit:samd:adafruit_feather_m0_express KeyboardAdapter21 && \
+python3 uf2conv.py -c -o KeyboardAdapter21.uf2 KeyboardAdapter21.ino.bin && \
+rm KeyboardAdapter21.ino*
 
-python3 uf2conv.py -c -o KeyboardAdapter21.uf2 KeyboardAdapter21.ino.bin
