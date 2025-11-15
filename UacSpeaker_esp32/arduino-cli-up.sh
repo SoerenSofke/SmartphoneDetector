53fff0e6c001f4bd8aded57ebe4d3b9575352e32
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2025-11-15 14:58:11 +0000

    Downgrade to 2.x for compatibility with library

--- UacSpeaker_esp32/arduino-cli-up.sh
+++ UacSpeaker_esp32/arduino-cli-up.sh
@@ -8,7 +8,10 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 
 # ESP32 Core installieren
 ./bin/arduino-cli core search --all
-./bin/arduino-cli core install esp32:esp32@3.3.3
+## 2.0.x is required for ESP32_USB_STREAM@0.1.0 (esp32:esp32@3.x is not supported for now by library)
+./bin/arduino-cli core install esp32:esp32@2.0.17
+## pyserial is required by esp32:esp32@2.x
+python3 -m pip install pyserial
 
 # Bibliotheken suchen
 ./bin/arduino-cli lib search "ESP32_USB_STREAM"
@@ -17,4 +20,4 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 ./bin/arduino-cli lib install ESP32_USB_STREAM@0.1.0
 
 # Bibliotheken auflisten
-./bin/arduino-cli lib install
+./bin/arduino-cli lib list
