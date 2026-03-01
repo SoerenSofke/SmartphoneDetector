11cef69f052b01c1d8776f8fa41d260d1236c715
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-03-01 10:15:47 +0000

    Update make-KeyboardAdapter21.sh to compile with Seeed XIAO M0 board configuration

--- KeyboardAdapter21_samd21-usbhost/make-KeyboardAdapter21.sh
+++ KeyboardAdapter21_samd21-usbhost/make-KeyboardAdapter21.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-arduino-cli compile --output-dir . --fqbn adafruit:samd:adafruit_feather_m0_express KeyboardAdapter21 && \
+./bin/arduino-cli compile --output-dir . --fqbn Seeeduino:samd:seeed_XIAO_m0 KeyboardAdapter21 && \
 python3 uf2conv.py -c -o KeyboardAdapter21.uf2 KeyboardAdapter21.ino.bin && \
 rm KeyboardAdapter21.ino*
 
