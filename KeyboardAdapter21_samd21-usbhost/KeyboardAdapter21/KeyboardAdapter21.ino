0389cccf3906b991a4b0ca97f1a0c771601b6fa1
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-03-01 16:33:58 +0000

    Refactor KeyboardAdapter21.ino for code clarity and organization

--- KeyboardAdapter21_samd21-usbhost/KeyboardAdapter21/KeyboardAdapter21.ino
+++ KeyboardAdapter21_samd21-usbhost/KeyboardAdapter21/KeyboardAdapter21.ino
@@ -1,16 +1,12 @@
-
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
-#pragma message("__cplusplus: " TOSTRING(__cplusplus))
-
-
-#define LED_ON  LOW
+#define LED_ON LOW
 #define LED_OFF HIGH
 
-#include<KeyboardController.h>
+#include <KeyboardController.h>
+#include <Adafruit_CH9328.h>
+
+Adafruit_CH9328 hid;
 
 USBHost usb;
-
 KeyboardController keyboard(usb);
 
 const int ledPin = 13;
@@ -18,11 +14,16 @@ const int ledPin = 13;
 void setup()
 {
   pinMode(ledPin, OUTPUT);
+  
+  Serial1.begin(9600);
+  while (!Serial1) delay(100);
+  hid.begin(&Serial1);
 }
 
 void keyPressed()
 {
   digitalWrite(ledPin, LED_ON);
+  hid.typeString("a");
 }
 
 void keyReleased()
