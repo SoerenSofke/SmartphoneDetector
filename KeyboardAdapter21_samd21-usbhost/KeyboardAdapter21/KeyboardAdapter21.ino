ddbaa5d2670eb25634dea7db92c954e2d69bbb5d
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-03-04 19:58:24 +0000

    Update key mappings in getLayer0 function for improved key layout

--- KeyboardAdapter21_samd21-usbhost/KeyboardAdapter21/KeyboardAdapter21.ino
+++ KeyboardAdapter21_samd21-usbhost/KeyboardAdapter21/KeyboardAdapter21.ino
@@ -1,11 +1,35 @@
 #define LED_ON LOW
 #define LED_OFF HIGH
 
-#define KEY_A           0x04
 #define KEY_ENTER       0x28
 #define KEY_BACKSPACE   0x2A
 
-
+#define KEY_A           0x04
+#define KEY_B           0x05
+#define KEY_C           0x06
+#define KEY_D           0x07
+#define KEY_E           0x08
+#define KEY_F           0x09
+#define KEY_G           0x0A
+#define KEY_H           0x0B
+#define KEY_I           0x0C
+#define KEY_J           0x0D
+#define KEY_K           0x0E
+#define KEY_L           0x0F
+#define KEY_M           0x10
+#define KEY_N           0x11
+#define KEY_O           0x12
+#define KEY_P           0x13
+#define KEY_Q           0x14
+#define KEY_R           0x15
+#define KEY_S           0x16
+#define KEY_T           0x17
+#define KEY_U           0x18
+#define KEY_V           0x19
+#define KEY_W           0x1A
+#define KEY_X           0x1B
+#define KEY_Y           0x1C
+#define KEY_Z           0x1D
 
 #include <KeyboardController.h>
 #include <Adafruit_CH9328.h>
@@ -29,9 +53,9 @@ inline const uint8_t *getLayer0()
     memset(layer, 0x00, sizeof(layer));
 
     // clang-format off
-    layer[110] = KEY_A; layer[103] = KEY_A; layer[102] = KEY_A; layer[57] = KEY_A; layer[56] = KEY_A; layer[49] = KEY_BACKSPACE;
-    layer[109] = KEY_A; layer[104] = KEY_A; layer[101] = KEY_A; layer[48] = KEY_A; layer[55] = KEY_A; layer[50] = KEY_ENTER;
-    layer[108] = KEY_A; layer[105] = KEY_A; layer[100] = KEY_A; layer[97] = KEY_A; layer[54] = KEY_A; layer[51] = KEY_A;
+    layer[110] = KEY_J; layer[103] = KEY_L; layer[102] = KEY_U; layer[57] = KEY_Y; layer[56] = KEY_A; layer[49] = KEY_BACKSPACE;
+    layer[109] = KEY_H; layer[104] = KEY_N; layer[101] = KEY_E; layer[48] = KEY_I; layer[55] = KEY_O; layer[50] = KEY_ENTER;
+    layer[108] = KEY_K; layer[105] = KEY_M; layer[100] = KEY_A; layer[97] = KEY_A; layer[54] = KEY_A; layer[51] = KEY_A;
     layer[107] = KEY_A; layer[106] = KEY_A; layer[ 99] = KEY_A; layer[98] = KEY_A; layer[53] = KEY_A; layer[52] = KEY_A;
     // clang-format on
 
