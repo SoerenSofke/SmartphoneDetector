657fd2d927aac0774e22a47f90f724bbb9ee4a67
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-03-29 07:10:40 +0000

    Add arduino-cli setup script for ESP32-P4 board installation

---
+++ AnnoSynth164/arduino-cli-up.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Arduino CLI installieren
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+
+# Board informartion: https://www.waveshare.com/wiki/ESP32-P4-Pico
+./bin/arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+
+# Index aktualisieren und Paket installieren
+./bin/arduino-cli core update-index
+./bin/arduino-cli core install esp32:esp32@3.3.7
+
+# search ESP32-P4 (Pico) boards: ESP32P4 Dev Module: esp32:esp32:esp32p4
+./bin/arduino-cli board listall esp32:esp32:esp32p4
