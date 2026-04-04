3ab3a951c803571b128c85bf74bbfcf2d7f41983
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-04-04 14:44:53 +0000

    Refactor logging in Synth.ino to use tsprint function for consistent timestamped output

--- AnnoSynth164/Synth/Synth.ino
+++ AnnoSynth164/Synth/Synth.ino
@@ -2,16 +2,25 @@
 
 UsbMidi usbMidi;
 
+void tsprint(const char* msg) {
+    unsigned long ms = millis();
+    unsigned long s  = ms / 1000;
+    unsigned long m  = s / 60;
+    unsigned long h  = m / 60;
+    Serial.printf("[%02lu:%02lu:%02lu.%03lu] %s\r\n",
+                  h % 100, m % 60, s % 60, ms % 1000, msg);
+}
+
 void onMidiMessage(const uint8_t (&data)[4]) {
-    Serial.println("MIDI Message Received");
+    tsprint("MIDI Message Received");
 }
 
 void onDeviceConnect() {
-    Serial.println("MIDI Device Connected");
+    tsprint("MIDI Device Connected");
 }
 
 void onDeviceDisconnected() {
-    Serial.println("MIDI Device Disconnected");
+    tsprint("MIDI Device Disconnected");
 }
 
 void setup() {
