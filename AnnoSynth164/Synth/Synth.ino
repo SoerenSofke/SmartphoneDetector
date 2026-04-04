e9ec4b287d225e02eaa10b775f990cad0a7672f1
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-04-04 12:47:50 +0000

    Add UsbMidi class and implement MIDI message handling for ESP32

--- AnnoSynth164/Synth/Synth.ino
+++ AnnoSynth164/Synth/Synth.ino
@@ -1,11 +1,24 @@
-void setup()
-{
-    Serial.begin(115200);
-    delay(2000);
+#include "UsbMidi.h"
+
+UsbMidi usbMidi;
+
+void onMidiMessage(const uint8_t (&data)[4]) {
+    Serial.println("MIDI Message Received");
 }
 
-void loop()
-{
-    Serial.println("Hello World vom ESP32!");
-    delay(1000);
+void onDeviceConnect() {
+    Serial.println("MIDI Device Connected");
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);        
+
+    usbMidi.onMidiMessage(onMidiMessage);
+    usbMidi.onDeviceConnected(onDeviceConnect);
+    usbMidi.begin();
+}
+
+void loop() {
+    usbMidi.update();
 }
