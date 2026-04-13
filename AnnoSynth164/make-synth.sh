75c38171bd5bde017912ea3e3f23e40796860907
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-04-13 17:39:25 +0000

    Refactor make-synth.sh to improve compile command readability with multiline formatting

--- AnnoSynth164/make-synth.sh
+++ AnnoSynth164/make-synth.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-./bin/arduino-cli compile --output-dir . --fqbn esp32:esp32:esp32s3 Synth
+./bin/arduino-cli compile \
+    --output-dir . \
+    --fqbn esp32:esp32:esp32s3 \
+    --build-property "compiler.cpp.extra_flags=-Wa,-I{build.source.path}" \
+    --build-property "compiler.c.extra_flags=-Wa,-I{build.source.path}" \
+    Synth
 rm Synth.ino.bin
 rm Synth.ino.bootloader.bin
 rm Synth.ino.elf
