42cffa2223c21c16a4dbad35fc8f81b6fff91c0a
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2026-04-20 20:22:01 +0000

    Add linker flags for optimization in make-synth.sh to enhance build performance

--- AnnoSynth164/make-synth.sh
+++ AnnoSynth164/make-synth.sh
@@ -4,8 +4,9 @@
     --output-dir . \
     --fqbn esp32:esp32:esp32s3:FlashSize=8M,PartitionScheme=custom \
     --build-property "upload.maximum_size=8323072" \
-    --build-property "compiler.cpp.extra_flags=-Wa,-I{build.source.path}" \
-    --build-property "compiler.c.extra_flags=-Wa,-I{build.source.path}" \
+    --build-property "compiler.cpp.extra_flags=-Wa,-I{build.source.path} -flto" \
+    --build-property "compiler.c.extra_flags=-Wa,-I{build.source.path} -flto" \
+    --build-property "compiler.c.elf.extra_flags=-flto" \
     Synth
 rm Synth.ino.bin
 rm Synth.ino.bootloader.bin
